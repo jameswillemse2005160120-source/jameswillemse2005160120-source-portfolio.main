9d81ae32e27db07ccd0eb8e71fa3b72efb4b440b
jameswillemse2005160120-source <jameswillemse2005160120@gmail.com>
2025-12-09 14:36:51 +0200

    Update practice.js

--- chapter1/practice.js
+++ chapter1/practice.js
@@ -1,11 +1,9 @@
-// Subtracts 37 from 2000 and prints the result
+ Subtracts 37 from 2000 and prints the result
 console.log(2000 - 37);
-
-// Multiplies 30 by 27 and prints the result
+ Multiplies 30 by 27 and prints the result
 console.log(30 * 27);
-
-// Divides 120 by 20 and prints the result
+ Divides 120 by 20 and prints the result
 console.log(120 / 20);
-
-// Concatenates the strings "Your name", a space, and "is learning JavaScript!", then prints the result
+ Concatenates the strings "Your name", a space, and "is learning JavaScript!", then prints the result
 console.log("James" + " " + "is learning JavaScript!");
+
